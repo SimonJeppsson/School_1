b37981b1b41b29f78f02577e0c37286d2b9ae84f
SimonJeppsson <Simon1jeppsson@gmail.com>
2024-12-02 11:33:58 +0100

    Commit last changes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let x = 1000; 
-let y = 3; 
-var operation = prompt("enter + - * /");
+//let x = 1000; 
+//let y = 3; 
+//var operation = prompt("enter + - * /");
 
-if (operation === "+" ) {
-    console.log("The result of x+y is " + [x+y]);
-} 
-else if (operation === "-" ) {
-    console.log("The result of x-y is " + [x-y]);
-}
-else if (operation === "*"){
-    console.log("The result of x*y is " + [x*y]);
-} 
-else if (operation === "/"){
-    console.log("The result of x/y is " + [x/y]);
-}
-else { console.log("Wrong entery, enter + - * / ")}
+//if (operation === "+" ) {
+//    console.log("The result of x+y is " + [x+y]);
+//} 
+//else if (operation === "-" ) {
+//    console.log("The result of x-y is " + [x-y]);
+//}
+//else if (operation === "*"){
+//    console.log("The result of x*y is " + [x*y]);
+//} 
+//else if (operation === "/"){
+//    console.log("The result of x/y is " + [x/y]);
+//}
+//else { console.log("Wrong entery, enter + - * / ")}
 
 
 
